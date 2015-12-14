b584bfe0b877a1031bae7eebe4a8aa33018258b1
pedersen <jtp@cs.au.dk>
2015-12-14 23:43:37 +0100

    adding a bottom to the box

--- lito.py
+++ lito.py
@@ -8,10 +8,11 @@ from PIL import ImageFilter
 
 class heightmap(object):
 
-    def __init__(self, img, xyscale = 1.0, zscale = 100.0):
+    def __init__(self, img, xyscale = 1.0, zscale = 100.0, zoffset = 10.0):
         self.img = img.convert(mode="L")
         self.xyscale = xyscale
         self.zscale = zscale
+        self.zoffset = zoffset
         self.faces = []
         self.vertices = []
         self.addVertices()
@@ -24,9 +25,12 @@ class heightmap(object):
                 xf = float(x) * self.xyscale
                 yf = float(y) * self.xyscale
                 val = pixels[x,y]
-                zf = float(val)/255.0 * self.zscale
-                self.vertices.append( (xf,yf,zf) )
+                zf = float(val)/255.0 * self.zscale + self.zoffset
+                self.addVertex( (xf,yf,zf) )
     
+    def addVertex(self, v):
+        self.vertices.append(v)
+
     def triangulate(self):
         w,h = self.img.size
         for y in range(1, h):
@@ -38,6 +42,49 @@ class heightmap(object):
                 self.addFace(prev_xy, prev_y, cur)
                 self.addFace(prev_xy, cur, prev_x)
 
+    def boxify(self):
+        """turn into a box
+
+        for each outer edge create lower version at (x,y,0)
+        
+        join all to a center vertex
+
+        0 1 2 3
+        8     9
+        4 5 6 7
+
+        """
+        w,h = self.img.size
+        self.addSkirt( [ i for i in range(w)])
+        self.addSkirt( [ i + w*(h-1) for i in range(w)])
+        self.addSkirt( [ j*w for j in range(h)], stride = w)
+        self.addSkirt( [ j*w + w-1 for j in range(h)], stride = w)
+
+        #add a bottom
+        self.addVertex( (w/2, h/2, 0) )
+        c = len(self.vertices)-1
+        for idx in range(w*h, c):
+            self.addFace(idx, idx -1, c)
+            
+    def addSkirt(self, top_indicies, stride = 1):
+        for i in range(len(top_indicies)):
+            idx = top_indicies[i]
+            x,y,z =  self.vertices[idx] 
+            self.addVertex(  (x,y, 0) )
+            if i > 0:
+                self.addBackQuad(idx, stride)
+
+    def addBackQuad(self, top, stride):
+        """ given to indices above eachother 
+        create a quad 
+        the last index is the last in the list of vertices
+        """
+        bottom = len(self.vertices)-1
+        self.addFace(top, top-stride, bottom)
+        self.addFace(top-stride, bottom-1, bottom)
+        
+
+        
     def addFace(self, v1, v2, v3):
         """add one for objs sake"""
         self.faces.append ( (v1+1, v2+1, v3+1) )
@@ -65,6 +112,7 @@ if __name__ == "__main__":
 
     hm = heightmap(img)
     hm.triangulate()
+    hm.boxify()
     hm.store("lito.obj")
 
     input("Await keypress...")
