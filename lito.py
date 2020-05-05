8c3778d5c934b6fb9d67a8582085dc443c221409
jacob toft pedersen <jacob@rotand.dk>
2020-05-05 22:49:16 +0200

    Update to use skikit-image
    This means beter handling of 16bit gray scale images
    Also added a step to rescale the image before generating the obj, this will add some smoothing and
    makes the sampling better.

--- lito.py
+++ lito.py
@@ -5,7 +5,7 @@ Usage:
   lito [options] <image> <outfile>
 
 Options:
-  -d --dimension=DIMENSION  Maximal dimension [default: 10.0] 
+  -d --dimension=DIMENSION  Maximal dimension [default: 10.0]
   -z --zoffset=ZOFFSET      Padding in bottom to avoid holes [default: 0.3]
   -s --zscale=ZSCALE        Maximal height in [default: 2.5]
   -h  -help                 This screen
@@ -14,40 +14,40 @@ Options:
 """
 
 import sys, os, codecs
-from PIL import Image
-from PIL import ImageFilter
+from skimage import io, transform
+import numpy
+
 from docopt import docopt
-import pprint 
+import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 
 class heightmap(object):
 
-    def __init__(self, img, xyscale, zscale, zoffset):
-        self.img = img.convert(mode="L")
-        self.xyscale = xyscale
-        self.zscale = zscale / 255.0
+    def __init__(self, img,  z_max, zoffset):
+        self.img = img
         self.zoffset = zoffset
+        self.z_max = z_max
         self.faces = []
         self.vertices = []
+        self.max_h = 0
         self.addVertices()
-        
+
+
     def addVertices(self):
-        pixels = self.img.load()
-        w,h = self.img.size
+        w,h = self.img.shape
         for y in range(h):
             for x in range(w):
-                xf = float(x) * self.xyscale
-                yf = float(h - y - 1) * self.xyscale
-                val = float(255 - pixels[x,y])
-                zf = val * self.zscale
-                self.addVertex( (xf,yf,zf) )
-    
+                xf = float(x)
+                yf = float(y)
+                self.addVertex( (xf,yf,self.img[x][y]) )
+
     def addVertex(self, v):
+        self.max_h = max(self.max_h, v[2])
         self.vertices.append(v)
 
     def triangulate(self):
-        w,h = self.img.size
+        w,h = self.img.shape
         for y in range(1, h):
             for x in range(1, w):
                 prev_x  = (x-1) + y     * w
@@ -69,18 +69,18 @@ class heightmap(object):
         and add a quad if possible
         # TODO: this does create a few duplicate vertices, 
         """
-        w,h = self.img.size
+        w,h = self.img.shape
         self.addSkirt( [ i for i in range(w)])
         self.addSkirt( [ i + w*(h-1) for i in range(w)])
         self.addSkirt( [ j*w for j in range(h)], stride = w)
         self.addSkirt( [ j*w + w-1 for j in range(h)], stride = w)
 
         self.addBottom()
-            
+
     def addSkirt(self, top_indicies, stride = 1):
         for i in range(len(top_indicies)):
             idx = top_indicies[i]
-            x,y,z =  self.vertices[idx] 
+            x,y,z =  self.vertices[idx]
             self.addVertex(  (x,y, -self.zoffset) )
             if i > 0:
                 self.addBackQuad(idx, stride)
@@ -93,10 +93,10 @@ class heightmap(object):
         bottom = len(self.vertices)-1
         self.addFace(top, top-stride, bottom)
         self.addFace(top-stride, bottom-1, bottom)
-        
+
     def addBottom(self):
-        w,h = self.img.size
-        self.addVertex( (self.xyscale * w/2, self.xyscale * h/2, -self.zoffset) )
+        w,h = self.img.shape
+        self.addVertex( ( w/2,  h/2, -self.zoffset) )
         c = len(self.vertices)-1
         for idx in range(w*h, c):
             self.addFace(idx, idx -1, c)
@@ -104,7 +104,7 @@ class heightmap(object):
     def store(self, fn):
         with codecs.open(fn, 'w', 'utf-8') as out:
             for v in self.vertices:
-                out.write("v {} {} {}\n".format(*v))
+                out.write("v {} {} {}\n".format(v[0], v[1], self.z_max * v[2] / self.max_h))
             out.write("\n")
             for f in self.faces:
                 out.write("f {} {} {}\n".format(*f))
@@ -119,15 +119,13 @@ if __name__ == "__main__":
     zoffset = float(args['--zoffset'])
     zscale = float(args['--zscale'])
     outfile = args['<outfile>']
-    img = Image.open(fn)
-    #.filter(ImageFilter.EDGE_ENHANCE).filter(ImageFilter.MedianFilter(5))
-    #.filter(ImageFilter.GaussianBlur())
+    img = io.imread(fn, as_gray = True)
 
-#    img.show()
-    xyscale = dim / float(max(*img.size))
-    print("image : {}x{}".format(*img.size))
+    xyscale = dim / float(max(img.shape))
+    print("image : {}x{}".format(*img.shape))
     print("Scaling xy:{} z:{} ".format(xyscale, zscale))
-    hm = heightmap(img, xyscale = xyscale, zoffset = zoffset , zscale = zscale)
+    img = transform.rescale(img, xyscale, anti_aliasing = True)
+    hm = heightmap( img, zoffset = zoffset , z_max = zscale)
     hm.triangulate()
     hm.boxify()
     hm.store(outfile)
